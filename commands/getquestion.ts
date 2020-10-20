8ace5f141adf4b090019b4c2af07edf713eac453
cheesetheorange <67130400+cheesetheorange@users.noreply.github.com>
2020-10-19 22:56:53 -0700

    accept multiple choice letters and clean up some stuff

--- commands/getquestion.ts
+++ commands/getquestion.ts
@@ -8,14 +8,19 @@ export default {
     alias: new Set(["question","newquestion","q"]),
     run:async (command_parsed:command_parsed_output, msg:Message, client:my_client)=>{
         const question = await get_question()
-        await msg.reply(`question:${question.tossup_question}`)
+        const firstLetter = question.tossup_answer.charAt(0).toLowerCase()
+        await msg.reply(`question:\n${question.tossup_question}`)
+        console.log(question.tossup_answer)
         //@ts-ignore
         const collector:MessageCollector = new Discord.MessageCollector(msg.channel, m => m.author.id === msg.author.id, { time: 5e3 });
         collector.on('collect', async(message:Message) => {
             if(message.content === question.tossup_answer){
                 msg.reply("correct")
+            } else if((firstLetter === "w" || firstLetter === "x" || firstLetter === "y" || firstLetter === "z") && message.content.toLowerCase() === firstLetter) {
+                msg.reply("correct")
+            } else{
+                msg.reply("incorrect: " + question.tossup_answer)
             }
-            msg.reply("incorrect"+question.tossup_answer)
         })
     }
 }

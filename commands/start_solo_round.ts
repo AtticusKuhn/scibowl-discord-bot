bfa1450ed414fea4b1b465f6cd436f48c193f5c9
eulerthedestroyer <eulerthedestroyer@gmail.com>
2020-10-19 19:03:41 -0700

    did solo round bonus

--- commands/start_solo_round.ts
+++ commands/start_solo_round.ts
@@ -1,8 +1,20 @@
 import {json_embed} from "../discord_utils/embeds"
 import {Message, MessageCollector} from "discord.js"
-import {my_client, command_parsed_output} from "../types"
+import {my_client, command_parsed_output, database_response} from "../types"
 const Discord = require('discord.js');
 import {get_question} from "../methods"
+function async_collection(msg:Message,question:database_response){
+    return new Promise( (resolutionFunc,_) => {
+        //@ts-ignore
+        const collector:MessageCollector = new Discord.MessageCollector(msg.channel, m => m.author.id === msg.author.id, { time: 5e3 });
+        collector.on('collect', async(message:Message) => {
+            if(message.content === question.tossup_answer){
+                resolutionFunc({success:true, message})
+            }
+            resolutionFunc({success:false, message})
+        })
+    })
+}
 export default {
     description:"Get a random question",
     alias: new Set(["solo"]),
@@ -11,15 +23,20 @@ export default {
         for(let question_number =0;question_number<13;question_number++){
             const question = await get_question()
             await msg.reply(`question number ${question_number}:${question.tossup_question}`)
-            //@ts-ignore
-            const collector:MessageCollector = new Discord.MessageCollector(msg.channel, m => m.author.id === msg.author.id, { time: 5e3 });
-            collector.on('collect', async(message:Message) => {
-                if(message.content === question.tossup_answer){
-                    msg.reply(`correct ${score} points`)
-                    score++
+            const response:{success?:boolean, message?:Message} = await async_collection(msg, question)
+            if(response.success){
+                await msg.reply(`success, score is ${score++}`)
+                await msg.reply(`BONUS: ${question.bonus_question}`)
+                const bonus_response:{success?:boolean, message?:Message} = await async_collection(msg, question)
+                if(bonus_response.success){
+                    await msg.reply(`success, score is ${score++}`)
+                }else{
+                    await msg.reply(`incorrect: ${question.bonus_answer}`)
+
                 }
-                msg.reply("incorrect"+question.tossup_answer)
-            })
+            }else{
+                await msg.reply(`incorrect: ${question.tossup_answer}`)
+            }
         }
         return `Final score is ${score}`
     }

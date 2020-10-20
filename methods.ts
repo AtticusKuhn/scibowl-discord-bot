bc141aeee590e9470b8b9768cbd4702a27ea8628
cheesetheorange <67130400+cheesetheorange@users.noreply.github.com>
2020-10-20 10:02:24 -0700

    allow for multiple choice answers

--- methods.ts
+++ methods.ts
@@ -6,6 +6,21 @@ export async function get_question(){
     const question:database_response = ((await axios.get("https://scibowldb.com/api/questions/random")).data).question
     return question
 }
+
+export function check_answer(answer:string, response:string){
+    answer = answer.toLowerCase()
+    response = response.toLowerCase()
+    if (["w","x","y","z"].indexOf(answer.charAt(0)) > -1) {
+        if (response === answer.charAt(0)){
+            return true
+        } else if (response === answer.slice(answer.indexOf(" ") + 1)) {
+            return true
+        }
+
+    }
+    return response === answer;
+}
+
 export function async_collection(msg:Message, check:(m:Message)=>boolean, filter:(m:Message)=>boolean):Promise<{success?:boolean,message?:Message}>{
     return new Promise( (resolutionFunc,_) => {
         //@ts-ignore
